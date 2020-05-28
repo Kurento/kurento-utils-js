09cb2c8b54ef332b17cfc67700f3a61e0769b5c4
Jesús Leganés-Combarro 'piranna <piranna@gmail.com>
2020-05-28 22:04:37 +0200

    Transpile and publish version compatible with nuxt.js bundler

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,7 @@
 module.exports = {
-  entry: './index.mjs',
+  entry: './index.js',
   devtool: 'source-map',
   mode: 'production',
-  module: {
-    rules: [
-      {
-        test: /\.m?js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env'],
-            plugins: [
-              "@babel/plugin-proposal-class-properties",
-              "@babel/plugin-proposal-private-methods"
-            ]
-          }
-        }
-      }
-    ]
-  },
   output: {
     path: __dirname,
     filename: 'kurento-utils.js',
